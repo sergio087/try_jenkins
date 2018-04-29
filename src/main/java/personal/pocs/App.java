0993e660efae17708695e0dac983a84cdb9e935b
sergion <sergio.087@gmail.com>
2018-04-29 00:04:26 -0300

    Make as Maven project

---
+++ src/main/java/personal/pocs/App.java
@@ -0,0 +1,13 @@
+package personal.pocs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
